7e3ff3a0a61c807c8f3cc1a9126ae528280a0b2d
Life4gal <Life4gal@gmail.com>
2022-09-25 16:20:03 +0800

    `feat`: type vector(not implement yet)

---
+++ gsl/include/gsl/type/vector.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <span>
+#include <gsl/core/value.hpp>
+#include <gsl/type/type_descriptor.hpp>
+#include <gsl/utils/assert.hpp>
+
+namespace gal::gsl
+{
+	namespace core
+	{
+		class ModuleContext;
+	}
+
+	namespace type
+	{
+		class Vector
+		{
+			friend struct vector_descriptor;
+		public:
+			using data_type = char*;
+			using size_type = std::uint32_t;
+			using lock_type = std::uint32_t;
+			using flag_type = std::uint32_t;
+
+		private:
+			data_type data_{nullptr};
+			size_type size_{0};
+			size_type capacity_{0};
+			lock_type lock_{0};
+
+			union
+			{
+				flag_type flag;
+
+				flag_type shared : 1;
+				flag_type dead : 1;
+				// more flags?
+			};
+
+		public:
+			[[nodiscard]] constexpr auto size() const noexcept -> size_type { return size_; }
+			[[nodiscard]] constexpr auto capacity() const noexcept -> size_type { return capacity_; }
+			[[nodiscard]] constexpr auto is_locked() const noexcept -> lock_type { return lock_; }
+			[[nodiscard]] constexpr auto lock_count() const noexcept -> lock_type { return lock_; }
+
+			auto lock(core::ModuleContext& context) -> void;
+			auto unlock(core::ModuleContext& context) -> void;
+
+			auto reserve(core::ModuleContext& context, size_type new_capacity, type_descriptor::size_type type_size) -> void;
+			auto resize(core::ModuleContext& context, size_type new_size, type_descriptor::size_type type_size, bool zero_it) -> void;
+			auto clear(core::ModuleContext& context) -> void;
+		};
+
+		struct vector_descriptor : public type_descriptor
+		{
+			friend type_descriptor;
+
+		private:
+			[[nodiscard]] constexpr static auto type_size_impl() noexcept -> size_type { return sizeof(Vector); }
+
+			constexpr static auto zero_out_impl(void* dest, const size_type count) noexcept -> void
+			{
+				std::ranges::for_each(
+						static_cast<Vector*>(dest),
+						static_cast<Vector*>(dest) + count,
+						[](Vector& d) { d = {}; });
+			}
+
+			constexpr static auto copy_into_impl(void* dest, const void* source, const size_type count) -> void
+			{
+				const auto vector_source = std::span{static_cast<const Vector*>(source), count};
+				std::ranges::for_each(
+						vector_source,
+						[dest = static_cast<Vector*>(dest)](const auto& s) mutable
+						{
+							dest->size_ = s.size_;
+							dest->capacity_ = s.capacity_;
+							// todo: how about lock?
+							// dest->lock_ = s.lock_;
+							// todo: how about non-trivial type?
+							utils::gsl_assert(dest->data_ != nullptr, "dest vector should allocate memory before copy!");
+							std::ranges::copy(s.data_, s.data_ + s.size_, dest->data_);
+							// next one
+							++dest;
+						});
+			}
+
+			constexpr static auto move_into_impl(void* dest, void* source, const size_type count) -> void
+			{
+				auto vector_source = std::span{static_cast<Vector*>(source), count};
+				std::ranges::for_each(
+						vector_source,
+						[dest = static_cast<Vector*>(dest)](auto& s) mutable
+						{
+							dest->size_ = std::exchange(s.size_, 0);
+							dest->capacity_ = std::exchange(s.capacity_, 0);
+							// todo: how about lock?
+							// dest->lock_		= std::exchange(s.lock_, 0);
+							dest->data_ = std::exchange(s.data_, nullptr);
+							// next one
+							++dest;
+						});
+			}
+		};
+	}
+
+	namespace core
+	{
+		// todo
+		template<>
+		struct ValueCaster<type::Vector>
+		{
+			constexpr static auto from(const type::Vector data) noexcept -> Value = delete;
+
+			constexpr static auto to(const Value& data) noexcept -> type::Vector = delete;
+		};
+	}
+}
---
+++ gsl/src/type/vector.cpp
@@ -0,0 +1,72 @@
+#include <gsl/type/vector.hpp>
+
+namespace gal::gsl::type
+{
+	auto Vector::lock(core::ModuleContext& context) -> void
+	{
+		if (shared || dead) { return; }
+
+		++lock_;
+
+		// todo: context check lock overflow?
+		(void)context;
+	}
+
+	auto Vector::unlock(core::ModuleContext& context) -> void
+	{
+		if (shared || dead) { return; }
+
+		// todo: context check lock underflow?
+		(void)context;
+
+		--lock_;
+	}
+
+	auto Vector::reserve(core::ModuleContext& context, const size_type new_capacity, const type_descriptor::size_type type_size) -> void
+	{
+		if (is_locked())
+		{
+			// todo: cannot change a vector's capacity if it locked
+			return;
+		}
+
+		if (capacity_ >= new_capacity)
+		{
+			// nothing to do
+			return;
+		}
+
+		// todo: do memory allocate
+		(void)context;
+		(void)type_size;
+	}
+
+	auto Vector::resize(core::ModuleContext& context, const size_type new_size, const type_descriptor::size_type type_size, const bool zero_it) -> void
+	{
+		if (is_locked())
+		{
+			// todo: cannot change a vector's size if it locked
+			return;
+		}
+
+		if (new_size > capacity_)
+		{
+			// todo: grow vector
+		}
+
+		if (zero_it && new_size > size_) { std::memset(data_ + size_ * type_size, 0, (new_size - size_) * type_size); }
+		size_ = new_size;
+	}
+
+	auto Vector::clear(core::ModuleContext& context) -> void
+	{
+		if (is_locked())
+		{
+			(void)context;
+			// todo: cannot clear a vector if it locked
+			return;
+		}
+
+		size_ = 0;
+	}
+}
