86a760e03077bf56f88dbaf0d759242485a5c6e0
Life4gal <Life4gal@gmail.com>
2022-10-10 20:29:45 +0800

    config about heap memory

---
+++ gsl/include/gsl/config.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <type_traits>
+
+namespace gal::gsl
+{
+	namespace debug
+	{
+		auto freeze_here() -> void;
+	}
+
+	using heap_data_type = char*;
+	using heap_data_size_type = std::uint32_t;
+
+	template<typename SizeType, typename DataType>
+		requires std::is_integral_v<SizeType> && std::is_pointer_v<DataType>
+	class HeapSmallAllocationPolicy
+	{
+	public:
+		using size_type = SizeType;
+		using data_type = DataType;
+
+		constexpr static size_type size_type_size = sizeof(size_type);
+		constexpr static size_type size_type_bit_size = size_type_size * 8;
+		constexpr static size_type size_type_mask = size_type_bit_size - 1;
+		constexpr static size_type size_type_offset = size_type_size + 1;
+
+		[[nodiscard]] constexpr static auto get_fit_capacity(const size_type capacity) noexcept -> size_type { return (capacity + size_type_mask) & ~size_type_mask; }
+
+		[[nodiscard]] constexpr static auto get_descriptor_count(const size_type fit_capacity) noexcept -> size_type { return fit_capacity / 8; }
+
+		struct descriptor_state
+		{
+			size_type index;
+			size_type offset;
+			size_type mask;
+		};
+
+		constexpr static descriptor_state bad_descriptor{
+				.index = static_cast<size_type>(-1),
+				.offset = static_cast<size_type>(-1),
+				.mask = static_cast<size_type>(-1)};
+
+		[[nodiscard]] constexpr static descriptor_state get_descriptor_state(const data_type root, const size_type capacity, const size_type size_per_element, const data_type this_data) noexcept
+		{
+			std::ptrdiff_t test_index = (this_data - root) / size_per_element;
+			if (test_index < 0 || test_index >= capacity)
+			{
+				// todo: let it crash
+				return bad_descriptor;
+			}
+
+			const auto real_index = static_cast<size_type>(test_index);
+			const auto offset = real_index & size_type_mask;
+
+			return {.index = real_index >> size_type_offset, .offset = offset, .mask = size_type{1} << offset};
+		}
+	};
+
+	namespace heap
+	{
+		using data_type = heap_data_type;
+		using size_type = heap_data_size_type;
+
+		[[nodiscard]] auto allocate(size_type size) -> data_type;
+
+		auto deallocate(data_type data) -> void;
+
+		[[nodiscard]] auto allocated_size(data_type data) -> size_type;
+
+		template<typename T, typename... Args>
+		[[nodiscard]] auto new_object(Args&&... args) noexcept(std::is_nothrow_constructible_v<T, Args...>) -> T*
+		{
+			T* data = static_cast<T*>(allocate(sizeof(T)));
+			std::construct_at(data, std::forward<Args>(args)...);
+			return data;
+		}
+
+		template<typename T>
+		auto delete_object(T*& object) noexcept(std::is_nothrow_destructible_v<T>) -> void
+		{
+			std::destroy_at(object);
+			object = nullptr;
+		}
+	}
+}
--- gsl/include/gsl/core/macro.hpp
+++ gsl/include/gsl/core/macro.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #if defined(GAL_SCRIPT_LANG_COMPILER_MSVC) || defined(_MSC_VER)
+	#define GSL_MSVC
 	#define GSL_UNREACHABLE() __assume(0)
 	#define GSL_DEBUG_TRAP() __debugbreak()
 	#define GSL_IMPORTED_SYMBOL __declspec(dllimport)
@@ -11,7 +12,8 @@
 	#define GSL_DISABLE_WARNING_POP __pragma(warning(pop))
 	#define GSL_DISABLE_WARNING(warningNumber) __pragma(warning(disable \
 															: warningNumber))
-#elif defined(GAL_SCRIPT_LANG_COMPILER_GNU) || defined(GAL_SCRIPT_LANG_COMPILER_CLANG) || defined(__GNUC__) || defined(__clang__)
+#elif defined(GAL_SCRIPT_LANG_COMPILER_GNU) || defined(__GNUC__)
+	#define GSL_GNU
 	#define GSL_UNREACHABLE() __builtin_unreachable()
 	#define GSL_DEBUG_TRAP() __builtin_trap()
 	#define GSL_IMPORTED_SYMBOL __attribute__((visibility("default")))
@@ -23,6 +25,19 @@
 
 	#define GSL_PRIVATE_DO_PRAGMA(X) _Pragma(#X)
 	#define GSL_DISABLE_WARNING(warningName) GSL_PRIVATE_DO_PRAGMA(GCC diagnostic ignored #warningName)
+#elif defined(GAL_SCRIPT_LANG_COMPILER_CLANG) || defined(__clang__)
+	#define GSL_CLANG
+	#define GSL_UNREACHABLE() __builtin_unreachable()
+	#define GSL_DEBUG_TRAP() __builtin_trap()
+	#define GSL_IMPORTED_SYMBOL __attribute__((visibility("default")))
+	#define GSL_EXPORTED_SYMBOL __attribute__((visibility("default")))
+	#define GSL_LOCAL_SYMBOL __attribute__((visibility("hidden")))
+
+	#define GSL_DISABLE_WARNING_PUSH _Pragma("Clang diagnostic push")
+	#define GSL_DISABLE_WARNING_POP _Pragma("Clang diagnostic pop")
+
+	#define GSL_PRIVATE_DO_PRAGMA(X) _Pragma(#X)
+	#define GSL_DISABLE_WARNING(warningName) GSL_PRIVATE_DO_PRAGMA(Clang diagnostic ignored #warningName)
 #else
 	#define GSL_UNREACHABLE()
 	#define GSL_DEBUG_TRAP() 
