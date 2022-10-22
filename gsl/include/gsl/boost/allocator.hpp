389e094f27bde5f2775cf1c1ffc8b19130d88109
Life4gal <Life4gal@gmail.com>
2022-10-22 14:02:43 +0800

    `refactor`: **destructive refactoring**

--- gsl/include/gsl/boost/allocator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Do not include any header files, the allocator is always valid where this file is used.
-
-namespace gal::gsl::boost
-{
-	template<typename T>
-	using gsl_allocator = std::allocator<T>;
-}
