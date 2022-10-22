478a67937631a144391f7cf1b9a4bb019676b960
Life4gal <Life4gal@gmail.com>
2022-10-22 21:45:27 +0800

    value

---
+++ gsl/include/gsl/type/value.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace gal::gsl::type
+{
+	template<typename T>
+	class ValueCaster;
+
+	class Value
+	{
+	public:
+		union alignas(alignof(std::max_align_t))
+		{
+			char bits[16];
+			void* raw_pointer;
+			const void* raw_observer;
+			std::int32_t signed_integer_32[4];
+			std::uint32_t unsigned_integer_32[4];
+			std::int64_t signed_integer_64[2];
+			std::uint64_t unsigned_integer_64[2];
+			float single_precision[4];
+			double double_precision[2];
+		};
+
+		template<typename T>
+			requires ValueCaster<T>::value
+		[[nodiscard]] constexpr auto
+		as() const -> decltype(auto) { return ValueCaster<T>::to(*this); }
+	};
+
+	static_assert(sizeof(Value) == sizeof(std::uint32_t) * 4);
+	static_assert(sizeof(Value) == sizeof(float) * 4);
+
+	enum class value_caster_policy
+	{
+		UNDEFINED,
+		IMPLICIT,
+		SPECIFIED
+	};
+
+	struct value_caster_undefined
+	{
+		constexpr static value_caster_policy value = value_caster_policy::UNDEFINED;
+	};
+
+	struct value_caster_implicit
+	{
+		constexpr static value_caster_policy value = value_caster_policy::IMPLICIT;
+	};
+
+	struct value_caster_specified
+	{
+		constexpr static value_caster_policy value = value_caster_policy::SPECIFIED;
+	};
+
+	template<typename T>
+	class ValueCaster : public value_caster_undefined
+	{
+	public:
+		constexpr static auto from(const T&) -> Value = delete;
+		constexpr static auto to(const Value&) -> decltype(auto) = delete;
+	};
+
+	template<typename T>
+	class ValueCaster<const T> : public ValueCaster<T> { };
+
+	template<typename T>
+	class ValueCaster<T*> : public value_caster_implicit
+	{
+	public:
+		constexpr static auto from(const T* data) -> Value { return Value{.raw_observer = data}; }
+
+		constexpr static auto to(const Value& data) -> decltype(auto)
+		{
+			// not checked
+			return static_cast<T*>(data.raw_observer);
+		}
+	};
+
+	template<typename T>
+	class ValueCaster<const T*> : public value_caster_implicit
+	{
+	public:
+		constexpr static auto from(const T* data) -> Value { return Value{.raw_observer = data}; }
+
+		constexpr static auto to(const Value& data) -> decltype(auto)
+		{
+			// not checked
+			return static_cast<const T*>(data.raw_observer);
+		}
+	};
+
+	template<typename T>
+	class ValueCaster<T&> : public value_caster_implicit
+	{
+	public:
+		constexpr static auto from(const T& data) -> Value { return Value{.raw_observer = &data}; }
+
+		constexpr static auto to(const Value& data) -> decltype(auto)
+		{
+			// not checked
+			return *static_cast<T*>(data.raw_observer);
+		}
+	};
+
+	template<typename T>
+	class ValueCaster<const T&> : public value_caster_implicit
+	{
+	public:
+		constexpr static auto from(const T& data) -> Value { return Value{.raw_observer = &data}; }
+
+		constexpr static auto to(const Value& data) -> decltype(auto)
+		{
+			// not checked
+			return *static_cast<const T*>(data.raw_observer);
+		}
+	};
+}// namespace gal::gsl::core
