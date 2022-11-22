db06c47207732a9f9b1c932821d7975e3ba79c4e
life4gal <life4gal@gmail.com>
2022-11-22 23:18:56 +0800

    more simulate type

--- gsl/include/gsl/simulate/simulate.hpp
+++ gsl/include/gsl/simulate/simulate.hpp
@@ -5,6 +5,8 @@
 #include <gsl/container/vector.hpp>
 #include <gsl/type/value.hpp>
 #include <gsl/memory/allocator.hpp>
+#include <gsl/debug/assert.hpp>
+
 #include <span>
 #include <source_location>
 
@@ -44,6 +46,11 @@ namespace gal::gsl::simulate
 		value_type value_;
 
 	public:
+		// todo: ctor
+
+		[[nodiscard]] constexpr auto name() const noexcept -> name_type { return name_; }
+
+		[[nodiscard]] constexpr auto value() const noexcept -> value_type { return value_; }
 	};
 
 	class SimulateFunction final
@@ -58,9 +65,9 @@ namespace gal::gsl::simulate
 		size_type stack_size_;
 
 	public:
-		auto eval(SimulateContext& context) const -> SimulateNode::value_type { return code_->eval(context); }
-
 		[[nodiscard]] constexpr auto stack_size() const noexcept -> size_type { return stack_size_; }
+
+		auto eval(SimulateContext& context) const -> SimulateNode::value_type { return code_->eval(context); }
 	};
 
 	class SimulateContext
@@ -76,6 +83,8 @@ namespace gal::gsl::simulate
 
 		using function_container_type = std::span<SimulateFunction>;
 		using function_index_type = function_container_type::size_type;
+
+		constexpr static function_index_type invalid_function_index{static_cast<function_index_type>(-1)};
 
 		constexpr static std::size_t chunk_size{1024 * 1024};
 		using chunk_type = std::span<any_allocator_type::value_type, chunk_size>;
@@ -113,6 +122,12 @@ namespace gal::gsl::simulate
 		[[noreturn]] void throw_error(string::string_view message, const std::source_location& location = std::source_location::current());
 
 		auto call_function(function_index_type index, SimulateNode::value_type* args) -> SimulateNode::value_type;
+
+		[[nodiscard]] constexpr auto get_global_variable(const global_variable_index_type index) const -> const SimulateGlobalVariable& { return global_variables_[index]; }
+
+		[[nodiscard]] constexpr auto get_function_argument(const function_index_type index) const -> SimulateNode::value_type { return function_arguments_[index]; }
+
+		[[nodiscard]] constexpr auto get_stack_value(const stack_type::size_type index) const noexcept -> SimulateNode::value_type { return SimulateNode::value_type{.raw_pointer = stack_.data() + current_stack_used_ + index}; }
 	};
 
 	class SimulateFieldNode final : public SimulateNode
@@ -136,6 +151,89 @@ namespace gal::gsl::simulate
 		}
 	};
 
+	class SimulateLocalGetNode final : public SimulateNode
+	{
+	public:
+		using size_type = SimulateContext::stack_type::size_type;
+
+	private:
+		size_type index_;
+
+	public:
+		explicit SimulateLocalGetNode(const size_type index)
+			: index_{index} {}
+
+		auto eval(SimulateContext& context) -> value_type override { return context.get_stack_value(index_); }
+	};
+
+	class SimulateGlobalGetNode final : public SimulateNode
+	{
+	public:
+		using size_type = SimulateContext::global_variable_index_type;
+
+	private:
+		size_type index_;
+
+	public:
+		explicit SimulateGlobalGetNode(const size_type index)
+			: index_{index} {}
+
+		auto eval(SimulateContext& context) -> value_type override { return context.get_global_variable(index_).value(); }
+	};
+
+	class SimulateThisCallArgumentGet final : public SimulateNode
+	{
+	public:
+		using size_type = SimulateContext::function_index_type;
+
+	private:
+		size_type argument_index_;
+
+	public:
+		explicit SimulateThisCallArgumentGet(const size_type index)
+			: argument_index_{index} {}
+
+		void set_index(const size_type index) noexcept { argument_index_ = index; }
+
+		auto eval(SimulateContext& context) -> value_type override { return context.get_function_argument(argument_index_); }
+	};
+
+	class SimulateBlockNode : public SimulateNode
+	{
+	public:
+		using expressions_type = std::span<SimulateNode*>;
+
+	protected:
+		expressions_type expressions_;
+
+	public:
+		void set_expressions(const expressions_type expressions) { expressions_ = expressions; }
+
+		auto eval(SimulateContext& context) -> value_type override
+		{
+			for (const auto expression: expressions_) { expression->eval(context); }
+			return {};
+		}
+	};
+
+	class SimulateLetBlockNode final : public SimulateBlockNode
+	{
+	public:
+	private:
+		SimulateNode* expression_{nullptr};
+
+	public:
+		void set_expression(SimulateNode* expression) noexcept { expression_ = expression; }
+
+		auto eval(SimulateContext& context) -> value_type override
+		{
+			gsl_assert(expression_);
+
+			SimulateBlockNode::eval(context);
+			return expression_->eval(context);
+		}
+	};
+
 	class SimulateFunctionCallNode final : public SimulateNode
 	{
 	public:
@@ -151,6 +249,20 @@ namespace gal::gsl::simulate
 		post_eval_arguments_type post_arguments_;
 
 	public:
+		// construct a empty call
+		SimulateFunctionCallNode()
+			: function_index_{SimulateContext::invalid_function_index} {}
+
+		void build(
+				const index_type index,
+				const pre_eval_arguments_type pre_arguments,
+				const post_eval_arguments_type post_arguments)
+		{
+			function_index_ = index;
+			pre_arguments_ = pre_arguments;
+			post_arguments_ = post_arguments;
+		}
+
 		auto eval(SimulateContext& context) -> value_type override
 		{
 			for (argument_index_type i = 0; i < pre_arguments_.size(); ++i) { post_arguments_[i] = pre_arguments_[i]->eval(context); }
@@ -217,6 +329,66 @@ namespace gal::gsl::simulate
 		{
 			(void)context;
 			return value_;
+		}
+	};
+
+	class SimulateXValueCopyNode final : public SimulateNode
+	{
+	public:
+		using size_type = std::size_t;
+
+	private:
+		SimulateNode* lhs_;
+		SimulateNode* rhs_;
+		size_type size_;
+
+	public:
+		SimulateXValueCopyNode(
+				SimulateNode* lhs,
+				SimulateNode* rhs,
+				const size_type size
+				)
+			: lhs_{lhs},
+			  rhs_{rhs},
+			  size_{size} {}
+
+		auto eval(SimulateContext& context) -> value_type override
+		{
+			const auto l = lhs_->eval(context);
+			const auto r = rhs_->eval(context);
+
+			// todo: plain copy?
+			std::memcpy(l.raw_pointer, r.raw_observer, size_);
+			return l;
+		}
+	};
+
+	template<typename ValueType>
+	class SimulateLValueCopyNode final : public SimulateNode
+	{
+	public:
+		using value_type = ValueType;
+
+	private:
+		SimulateNode* lhs_;
+		SimulateNode* rhs_;
+
+	public:
+		SimulateLValueCopyNode(
+				SimulateNode* lhs,
+				SimulateNode* rhs)
+			: lhs_{lhs},
+			  rhs_{rhs} {}
+
+		auto eval(SimulateContext& context) -> SimulateNode::value_type override
+		{
+			const auto l = lhs_->eval(context);
+			const auto r = rhs_->eval(context);
+
+			auto* pl = type::ValueCaster<value_type*>::to(l);
+			auto* pr = type::ValueCaster<value_type*>::to(r);
+			*pl = *pr;
+			return l;
 		}
 	};
 }
